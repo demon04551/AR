ffcff5cff4d18e1a8dddc40e04bb542ec6877b40
zhangfengxiang <47386169@qq.com>
2016-04-05 20:51:27 +0800

    v1

---
+++ Assets/_scripts/Date/BuildingData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingType {
+	public int id;
+	public Vector3 position;
+	public Quaternion rotation;
+	public Vector3 scale;
+	public Color color;
+}
